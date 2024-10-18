ca22a192a8226c556a04573af21e4f459be1b275
angel <angel@artech.software>
2024-10-18 15:33:37 -0400

    feat update orderbook

--- script/CreateOrderBook.s.sol
+++ script/CreateOrderBook.s.sol
@@ -13,7 +13,7 @@ contract CreateOrderBooks is Script {
     OrderBook[] public orderBooks;
 
     // Dirección del contrato OrderBookFactory ya desplegado
-    address factoryAddress = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
+    address factoryAddress = 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9;
 
     struct OrderBook {
         address tokenA;
--- script/prepare.sh
+++ script/prepare.sh
@@ -11,13 +11,13 @@ echo "Deploy Token B"
 source .env && forge script script/DeployTokenB.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key $PRIVATE_KEY_TOKEN_B
 # Llamar función getOwner()
 echo "Get Owner"
-cast call 0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0 "getOwner()" --rpc-url http://127.0.0.1:8545
+cast call 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 "getOwner()" --rpc-url http://127.0.0.1:8545
 # Crear Orderbook
 echo "Create Orderbook"
 forge script script/CreateOrderBook.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key $PRIVATE_KEY
 # Verificar la lista de orderbooks
 echo "Get Orderbook list"
-cast call 0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0 "getKeysOrderBooks()(bytes32[])" --rpc-url http://127.0.0.1:8545
+cast call 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 "getKeysOrderBooks()(bytes32[])" --rpc-url http://127.0.0.1:8545
 # Obtener un libro especifico
 echo "Get Orderbook by ID"
-cast call 0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0 "getOrderBookById(bytes32)(address,address,bool,uint256,uint256)" "0xc404d413e92e9b151a97e823fece65248a27a8236ad3c9e8f92c46d3ab2d450d" --rpc-url http://127.0.0.1:8545
+cast call 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 "getOrderBookById(bytes32)(address,address,bool,uint256,uint256)" "0xc404d413e92e9b151a97e823fece65248a27a8236ad3c9e8f92c46d3ab2d450d" --rpc-url http://127.0.0.1:8545
