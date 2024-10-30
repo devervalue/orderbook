e1d23ea22ab1cf4b1dfcd5c88921eabf8c0a16bd
angel <angel@artech.software>
2024-10-29 21:35:54 -0400

    QueueLib.sol added code documentation and formatted code

--- src/QueueLib.sol
+++ src/QueueLib.sol
@@ -13,25 +13,55 @@ library QueueLib {
      *  @notice Structure of an order within the node.
      */
     struct Item {
+        /// @notice Unique identifier for the item
         bytes32 id;
+        /// @notice ID of the next item in the queue (0 if last)
         bytes32 next;
+        /// @notice ID of the previous item in the queue (0 if first)
         bytes32 prev;
     }
 
+    /**
+     * @notice Structure of the queue itself.
+     */
     struct Queue {
+        /// @notice ID of the first item in the queue (0 if empty)
         bytes32 first;
+        /// @notice ID of the last item in the queue (0 if empty)
         bytes32 last;
+        /// @notice Mapping from item ID to Item struct
         mapping(bytes32 => Item) items;
     }
 
+    /* View Functions */
+
+    /**
+     * @notice Check if an item exists in the queue.
+     * @param q The queue to check.
+     * @param _itemId The ID of the item to check.
+     * @return bool True if the item exists, false otherwise.
+     */
     function itemExists(Queue storage q, bytes32 _itemId) internal view returns (bool) {
+        if (_itemId == 0) return false;
         return q.items[_itemId].id != 0;
     }
 
+    /**
+     * @notice Check if the queue is empty.
+     * @param q The queue to check.
+     * @return bool True if the queue is empty, false otherwise.
+     */
     function isEmpty(Queue storage q) internal view returns (bool) {
         return q.first == 0;
     }
 
+    /* Mutative Functions */
+
+    /**
+     * @notice Push a new item to the end of the queue.
+     * @param q The queue to push to.
+     * @param _itemId The ID of the item to push.
+     */
     function push(Queue storage q, bytes32 _itemId) internal {
         if (_itemId == 0 || itemExists(q, _itemId)) revert QL__ItemAlreadyExists();
 
@@ -45,10 +75,14 @@ library QueueLib {
         q.last = _itemId;
     }
 
-    //Eliminar index
+    /**
+     * @notice Remove an item from the queue.
+     * @param q The queue to remove from.
+     * @param _itemId The ID of the item to remove.
+     */
     function remove(Queue storage q, bytes32 _itemId) internal {
-        if (isEmpty(q)) revert QL__EmptyQueue();
-        if (_itemId == 0 || !itemExists(q, _itemId)) revert QL__ItemDoesNotExist();
+        if (_itemId == 0 || isEmpty(q)) revert QL__EmptyQueue();
+        if (!itemExists(q, _itemId)) revert QL__ItemDoesNotExist();
 
         Item memory removedItem = q.items[_itemId];
         delete q.items[_itemId];
