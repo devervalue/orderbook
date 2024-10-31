b943b90bfec2d44593d65b4145ae651c8e724d70
angel <angel@artech.software>
2024-10-30 22:09:28 -0400

    security improvements of PairLib.sol

--- src/PairLib.sol
+++ src/PairLib.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "forge-std/console.sol";
 import "./OrderBookLib.sol";
 
